a70a87383d1931a54db66e24c13056d8e132b287
Vladimir Zlatanov <vlado@dikini.net>
2014-07-29 16:30:52 +0100

    add Prepare.R - install packages

---
+++ Prepare.R
@@ -0,0 +1,34 @@
+ 
+  
+#  Install rmarkdown from github
+devtools::install_github("rstudio/rmarkdown")
+
+#Install tufte R handout templates from github
+devtools::install_github("tufterhandout", "sachsmc")
+
+#Install Knitr
+devtools::install("knitr")
+
+#Install the grammar for graphics package
+devtools::install("ggplot2")
+
+#Install the grammar for graphics addon GGally package
+devtools::install("GGally")
+
+#Install the diagram package (optional)
+devtools::install("diagram")
+
+#Install the diagram package (optional)
+devtools::install("plyr")
+
+#Install igraph - graph an network analysis and manipulation package
+devtools::install(igraph)  # graph manipulation and anlysis
+
+#Install tm - the text mining package
+devtools::install("tm")
+
+#Install RTextTools - the other high level text mining package
+devtools::install("RTextTools")
+
+#Install matrixStats -  provides methods operating on rows and columns of matrices
+devtools::install("matrixStats")
